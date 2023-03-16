fab0355ff8713e8383fbeaf2d045dc03d0e8a064
mkg622 <127065275+mkg622@users.noreply.github.com>
2023-03-16 15:23:24 +0900

    .

--- c/gcc/lib/calc.c
+++ c/gcc/lib/calc.c
@@ -3,7 +3,7 @@
 
 void main() {
   int x, y;
-  printf("Input two numbers : ");
+  printf("Input two numbers : ")
   scanf("%d %d", &x, &y);
   print("%d + %d = %d\n", x, y, plus(x, y));
   print("%d - %d = %d\n", x, y, minus(x, y));
